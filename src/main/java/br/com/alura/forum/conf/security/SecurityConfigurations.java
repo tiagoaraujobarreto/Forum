1330e8f7bbe5d49495f328647112da3a9a8dd604
tiagoaraujobarreto <tiagoaraujo.barreto@gmail.com>
2021-12-06 19:51:49 -0300

    Liberando acesso aos endpoints públicos

--- src/main/java/br/com/alura/forum/conf/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/conf/security/SecurityConfigurations.java
@@ -1,11 +1,33 @@
 package br.com.alura.forum.conf.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
-	
+
+	// Configurações de autenticação
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	}
+
+	// Configuração de autorização
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers(HttpMethod.GET, "/topicos").permitAll()
+		.antMatchers(HttpMethod.GET, "/topicos/*").permitAll();
+	}
+
+	// Configuração de recursos estaticos9js, css, imagens, etc.)
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+	}
+
 }
